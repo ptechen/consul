69e213d44c4bfb8327bd057ae6621c545c14f65a
petechen <15736755067@163.com>
2020-06-25 17:37:23 +0800

    update

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,12 @@
-docker pull $1
+docker pull registry.cn-hangzhou.aliyuncs.com/huayong/alarm:latest
 docker stop  etl
 docker container prune -f
-docker run --name $2 -d \
+docker run --name alarm -d \
 -v /etc/localtime:/etc/localtime \
 -v /opt/log:/opt/log \
+-p 8081:8000 \
 --restart=always \
---network=host \
 --env CONSUL_HTTP_ADDR=127.0.0.1:8500 \
 --env SERVER_ADDR=127.0.0.1 \
---env SERVER_PORT=8080 \
-$1
+--env SERVER_PORT=8081 \
+registry.cn-hangzhou.aliyuncs.com/huayong/alarm:latest
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.4.2
 	github.com/hashicorp/consul/api v1.5.0
+	google.golang.org/grpc v1.30.0
 )
